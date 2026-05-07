253496935ea33e5a139e9a5ce6093dc4ba489dc8
Gabriel Johnson <gjohns57@vols.utk.edu>
2026-05-07 15:45:02 -0400

    Added optionallity to lab 9

--- parse_grades.py
+++ parse_grades.py
@@ -6,16 +6,23 @@ from datetime import date
 
 
 def lab_grade(grades: pd.DataFrame):
-    df = grades[(grades["Context"] == "Programming Assignments") & (grades["Score"] != "-")]
-    quiz = grades[(grades["Context"] == "Lab Quizzes") & (grades["Score"] != "-")]
+    exclude = {"Programming Assignment 9: Threaded Chat Server"}
+    df = grades[((grades["Context"] == "Programming Assignments") | (grades["Context"] == "Lab Quizzes")) & (grades["Score"] != "-") & ~grades["Name"].isin(exclude)]
+
+    if not grades[grades["Name"] == "Programming Assignment 9: Threaded Chat Server"].empty:
+        bonus_lab_score = pd.to_numeric(grades[(grades["Name"] == "Programming Assignment 9: Threaded Chat Server") & (grades["Score"] != "-")]["Score"]).sum()
+
+        min_lab_idx = pd.to_numeric(df["Score"]).idxmin()
+        if int(bonus_lab_score) > int(pd.to_numeric(df.loc[min_lab_idx, "Score"])):
+            print(df.loc[min_lab_idx, "Name"], "score of", df.loc[min_lab_idx, "Score"], "replaced with Lab 9 score of", bonus_lab_score)
+            df.loc[min_lab_idx, "Score"] = str(bonus_lab_score)
+
 
     current = pd.to_numeric(df["Score"]).sum()
-    quiz_score = pd.to_numeric(quiz["Score"]).sum()
     total = pd.to_numeric(df["Out Of"]).sum()
-    quiz_total = pd.to_numeric(quiz["Out Of"]).sum()
-    print("Programming Assignments:", current + quiz_score, "/", total + quiz_total, "=", (current + quiz_score) / (total + quiz_total))
+    print("Programming Assignments:", current, "/", total, "=", current / total)
 
-    return (current + quiz_score) / (total + quiz_total)
+    return current / total
 
 def exam_grade(grades: pd.DataFrame):
     exclude = {"Midterm Exam 1", "Midterm Exam 2", "Final Exam", "Final Exam Required"}
@@ -23,7 +30,7 @@ def exam_grade(grades: pd.DataFrame):
     current = pd.to_numeric(df["Score"]).sum()
     total = pd.to_numeric(df["Out Of"]).sum()
     print("Exams:", current, "/", total, "=", current / total)
-    
+
     return current / total
 
 def attendance_penalty(grades: pd.DataFrame):
@@ -42,14 +49,14 @@ def parse_html() -> pd.DataFrame:
         if os.path.splitext(file)[1] == ".html":
             fname = os.path.join(DIR, file)
             break
-    
+
     if fname == None:
         print("No grades html in directory")
         exit(1)
 
     with open(fname, "r", encoding="utf-8") as fin:
         contents = fin.read()
-    
+
     soup = BeautifulSoup(contents, 'html.parser')
 
 
@@ -64,7 +71,7 @@ def parse_html() -> pd.DataFrame:
         name_tag = title.find('a')
         if name_tag == None:
             continue
-    
+
         context_tag = title.find(class_="context")
         if context_tag == None:
             continue
@@ -73,24 +80,28 @@ def parse_html() -> pd.DataFrame:
         if grade_element == None: continue
 
         score_element = grade_element.find(class_="grade")
-        if score_element == None: continue
 
-        out_of_element = score_element.find_next_sibling()
-        if out_of_element == None: continue
+        score = "-"
+        if score_element != None:
+            out_of_element = score_element.find_next_sibling()
+            if out_of_element == None: continue
 
-        score_match = re.search("-|[0-9]+", score_element.text)
-        if score_match == None: continue
+            score_match = re.search("-|[0-9]+", score_element.text)
+            if score_match != None:
+                score = score_match[0]
+
 
         new_row = [
             name_tag.text,
             context_tag.text,
-            score_match[0],
+            score,
             out_of_element.text.strip()[2:]
         ]
 
 
         grades.loc[len(grades)] = new_row
-    
+
+    grades.loc[len(grades)] = ["Final Exam Curved Score", "Exams", "-", "100"]
     grades.to_csv("grades.csv")
     return grades
 
@@ -108,6 +119,3 @@ if __name__ == "__main__":
     total = lab_score * 0.4 + exam_score * 0.6 + attendance
 
     print("Total:", total)
-
-
-    
